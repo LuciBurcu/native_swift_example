cf70b8d0864523ac8085af958c9f047a073c6a4c
Lucian.Burcu <lucian.burcu@rebeldot.com>
2025-11-01 17:50:48 +0200

    Initial Commit

---
+++ native_swift_example/native_swift_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  native_swift_exampleApp.swift
+//  native_swift_example
+//
+//  Created by Lucian Burcu on 01.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct native_swift_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ native_swift_exampleTests/native_swift_exampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  native_swift_exampleTests.swift
+//  native_swift_exampleTests
+//
+//  Created by Lucian Burcu on 01.11.2025.
+//
+
+import Testing
+
+struct native_swift_exampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
